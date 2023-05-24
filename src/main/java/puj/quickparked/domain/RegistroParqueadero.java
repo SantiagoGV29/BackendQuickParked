1a83c417d2ab2e82c6fdf4f4d090d5d59891854e
SantiagoGV29 <gu_santiago@javeriana.edu.co>
2023-05-23 23:06:32 -0500

    hola

---
+++ src/main/java/puj/quickparked/domain/RegistroParqueadero.java
@@ -0,0 +1,65 @@
+package puj.quickparked.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import java.time.LocalDate;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class RegistroParqueadero {
+
+    @Id
+    @Column(
+        nullable = false,
+        updatable = false
+    )
+    @GeneratedValue(
+        strategy = GenerationType.IDENTITY
+    )
+    private Integer id;
+
+    @Column(nullable = false)
+    private LocalDate horaEntrada;
+
+    @Column(nullable = false)
+    private LocalDate horaSalida;
+
+    @Column
+    private LocalDate horaReserva;
+
+    @Column
+    private Double montoReserva;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuarioTrabajadorId", nullable = false)
+    private Usuario usuarioTrabajador;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "estadoRegistroParqueaderoId", nullable = false)
+    private EstadoRegistroParqueadero estadoRegistroParqueadero;
+
+    @OneToMany(mappedBy = "reserva")
+    private Set<Venta> reservaVentas;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sedeParqueaderoId", nullable = false)
+    private SedeParqueadero sedeParqueadero;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehiculoId", nullable = false)
+    private Vehiculo vehiculo;
+
+}
---
+++ src/main/java/puj/quickparked/repos/EstadoRegistroParqueaderoRepository.java
@@ -0,0 +1,10 @@
+package puj.quickparked.repos;
+
+import puj.quickparked.domain.EstadoRegistroParqueadero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EstadoRegistroParqueaderoRepository extends JpaRepository<EstadoRegistroParqueadero, Integer> {
+}
