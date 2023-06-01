28c4c115f17c0f13fc1712aa948b113d021c70d0
SantiagoGV29 <gu_santiago@javeriana.edu.co>
2023-06-01 15:21:49 -0500

    Despliegue

--- src/main/java/puj/quickparked/repos/ParqueaderoRepository.java
+++ src/main/java/puj/quickparked/repos/ParqueaderoRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface ParqueaderoRepository extends JpaRepository<Parqueadero, Integer> {
-    @Query(value = "SELECT p FROM Parqueadero p WHERE p.usuarioPropietario.id = :id")
+    @Query(value = "SELECT p FROM Parqueadero as p WHERE p.usuarioPropietario =  :id")
     List<Parqueadero> findAllByUsuarioPropietarioId(long id);
 }
